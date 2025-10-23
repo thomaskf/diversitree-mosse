514afcd9cf3cbc6a9e5f3e8c8cd0908649b99543
thomaskf <thomaskf@gmail.com>
2025-10-23 15:40:36 +1100

    Update the example

--- simple-test.R
+++ simple-test.R
@@ -9,10 +9,10 @@ sd <- 0.001
 # args <- list(lambda=1:4, mu=5, drift=6, diffusion=7)
 pars <- c(0.1,0.2,0,2.5,0.03, drift, diffusion)#y0,y1,x0,r,mu,drift,diffusion
 
-phy <- read.tree(text="(A:0.4,B:0.4);")
-types <- c(1,2)
+phy <- read.tree(text="((t0:0.2,t1:0.2):0.2,(t2:0.3,t3:0.3):0.1);")
+types <- c(1,2,3,4)
 names(types) <- phy$tip.label
-states <- c(0.15,0.1)
+states <- c(0.15,0.1,0.25,0.2)
 names(states) <- phy$tip.label
 states.sd <- rep(0.01,length(phy$tip.label))
 names(states.sd) <- phy$tip.label
